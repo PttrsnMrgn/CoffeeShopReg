f42ce7537d0a7ad76c2f313459eec78787c116a5
morgan <pttrsn.mrgn@gmail.com>
2022-09-16 22:42:53 -0500

    Completed CoffeeShopReg

---
+++ CoffeeShopReg/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using CoffeeShopReg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CoffeeShopReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private CoffeeShopRegContext context = new CoffeeShopRegContext();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult NewCustomer()
+        {
+            return View();
+        }
+
+        public IActionResult AddCustomerToDb(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+
+            return RedirectToAction("Welcome");
+        }
+
+        public IActionResult Welcome()
+        {
+            Customer customer = context.Customers.OrderBy(x => x.Id).LastOrDefault();
+            return View(customer);
+        }
+
+        public IActionResult CustomerInfo()
+        {
+            List<Customer> customers = context.Customers.ToList();
+            return View(customers);
+        }
+
+     
+
+
+    }
+}
---
+++ CoffeeShopReg/Models/CoffeeShopRegContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShopReg.Models
+{
+    public partial class CoffeeShopRegContext : DbContext
+    {
+        public CoffeeShopRegContext()
+        {
+        }
+
+        public CoffeeShopRegContext(DbContextOptions<CoffeeShopRegContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-RIU54O9;Initial Catalog=CoffeeShopReg; Integrated Security=SSPI\n;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.FirstName).HasMaxLength(30);
+
+                entity.Property(e => e.LastName).HasMaxLength(30);
+
+                entity.Property(e => e.Password).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
